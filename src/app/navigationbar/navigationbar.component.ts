c19fd47cc367a206045fd45dce535e6414dac04b
Jan Lindheimer <jan.lindheimer@hotmail.com>
2020-12-14 10:44:59 +0100

    Update

--- src/app/navigationbar/navigationbar.component.ts
+++ src/app/navigationbar/navigationbar.component.ts
@@ -28,7 +28,7 @@ export class NavigationbarComponent implements OnInit {
     });
     document.addEventListener('scroll', () => {
       var scroll_position = window.scrollY;
-      if (scroll_position > 250) {
+      if (scroll_position > 100) {
         header.style.backgroundColor = '#29323c';
       } else {
         header.style.backgroundColor = 'transparent';
--- src/app/section-hero/section-hero.component.spec.ts
+++ src/app/section-hero/section-hero.component.spec.ts
@@ -26,3 +26,5 @@ describe('SectionHeroComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
